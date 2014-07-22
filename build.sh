6236d5361d0010a79c41ace99ce6e111157855df
Sourabh Shirhatti <shirhatti@outlook.com>
2014-07-22 11:21:33 -0700

    Fixed Mac build system

--- build.sh
+++ build.sh
@@ -3,4 +3,18 @@
 ver=`cat ~/.kre/alias/default.alias`
 add_to_path="/Users/"$USER"/.kre/packages/"$ver"/bin"
 export PATH=$PATH:/usr/local/bin:$add_to_path
-k build
+directory="./"
+temp=$directory"project.json"
+counter=0
+# If unable to find project.json, keep going up a directory till project.json found
+while [ ! -f $temp ]
+do
+	let counter=counter+1
+	if [ $counter -gt 3 ]
+	then
+		break
+	fi
+	directory=$directory"../"
+	temp=$directory"project.json"
+done
+k build $directory
