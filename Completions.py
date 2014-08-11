c7126d7c24642eed11af1158bec9f1fbab418ffb
Sourabh Shirhatti <shirhatti@outlook.com>
2014-08-11 14:44:42 -0700

    Auto-complete update

--- Completions.py
+++ Completions.py
@@ -1,43 +1,29 @@
 import sublime
 import sublime_plugin
 import threading
-import urllib.request
+import urllib.request, urllib.parse
 import json
 import re
 
-# class Loading():
-#     def __init__(self, view, status_message, display_message, callback):
-#         self.view = view
-#         self.i = 0
-#         self.dir = 1
-#         self.status_message = status_message
-#         self.display_message = display_message
-#         self.callback = callback
-#     def increment(self):
-#         before = self.i % 8
-#         after = (7) - before
-#         if not after:
-#             self.dir = -1
-#         if not before:
-#             self.dir = 1
-#         self.i += self.dir
-#         self.view.set_status(self.status_message, " [%s=%s]" % \
-#                 (" " * before, " " * after))
-#         sublime.set_timeout(lambda: self.callback(), 100)
-#     def clear(self):
-#         self.view.erase_status(self.status_message)
-#         pass
-
+SETTINGS_FILE = 'Kulture.sublime-settings'
 AC_OPTS = sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS
+
+class KKultureComplete (sublime_plugin.TextCommand):
+    def run(self, edit, action = None, **args):
+        print("DOT_COMPLETE:::::Code Reached")
+        self.view.insert(edit, self.view.sel()[0].begin(), args['key'])
+        self.view.run_command('auto_complete')
+        return True
 
 class KKultureCompletion( sublime_plugin.EventListener):
     def __init__(self):
-        request = urllib.request.Request("https://www.myget.org/F/aspnetrelease/api/v2/Packages()?$select=Id&$format=json&orderby=DownloadCount&$top=100",
-            headers={"User-Agent": "Sublime"})
-        http_file = urllib.request.urlopen(request)
         self.result = {}
         self.cache = []
-        response = json.loads(http_file.read().decode('utf-8'))['d']
+        self.thread = RetrievePackageNames(5)
+        self.thread.start()
+        while(self.thread.is_alive()):
+            pass
+        response = self.thread.response
         for package in response:
             t = (package['Id'],package['Id'])
             # if t not in self.result:
@@ -53,7 +39,7 @@ class KKultureCompletion( sublime_plugin.EventListener):
                     self.result[match.group(1)].append((match.group(2), match.group(2)))
             except AttributeError as e:
                 pass
-        print("KULTURE::::Done caching")
+
     def on_query_completions(self, view, prefix, locations):
         pos = locations[0]
         scopes = view.scope_name(pos).split()
@@ -92,41 +78,60 @@ class KKultureCompletion( sublime_plugin.EventListener):
                 if index==pos:
                     if (depth == 1 and tokens[0] == 'dependencies'):
                         # Version number
-                        version_regex = r'(?:,|{)"([-a-zA-Z0-9.*]+)":"[-a-zA-Z0-9.*]*$'
+                        version_regex = r'(?:,|{|\[])"([-a-zA-Z0-9.*]+)":"[-a-zA-Z0-9.*]*$'
                         try:
                             package_name = re.search(version_regex, doc[0:index]).group(1)
-                            return self.result[package_name]
+                            return (self.result[package_name], AC_OPTS)
                         except AttributeError as e:
                             pass
                         except KeyError as e:
-                            return []
+                            # TODO: Not in cache. Make HTTP request to feth completions on that package
+                            return AC_OPTS
                         # Package name
-                        return self.cache
-                    break
-            return (self.result['EntityFramework'], AC_OPTS)
+                        return (self.cache, AC_OPTS)
+                    elif depth == 0:
+                        # In future, get completions dynamically from http://schemastore.org/schemas/json/project
+                        # Replace word completions with snippets
+                        return ([('version', 'version" : "$1"'),
+                                    ('dependencies', 'dependencies" : {\n\t"$1": "$2"\n}'),
+                                    ('commands', 'commands" : {\n\t"$1": "$2"\n}'),
+                                    ('configurations', 'configurations" : {\n\t"$1": {}\n}'),
+                                    ('compilationOptions', 'compilationOptions'),
+                                    ('frameworks', 'frameworks" : {\n\t"$1": "$2"\n}'),
+                                    ('description', 'description" : "$1"'),
+                                    ('authors', 'authors" : [\n\t"$1"\n]'),
+                                    ('code', 'code" : {\n\t"$1": "$2"\n}'),
+                                    ('shared', 'shared" : {\n\t"$1": "$2"\n}'),
+                                    ('exclude', 'exclude" : {\n\t"$1": "$2"\n}'),
+                                    ('preprocess', 'preprocess" : {\n\t"$1": "$2"\n}'),
+                                    ('resources', 'resources')
+                                ], AC_OPTS)
+                    else:
+                        break
+
+            return AC_OPTS
 
 
+class RetrievePackageNames(threading.Thread):
+    def __init__(self,timeout):
+        self.timeout = timeout
+        self.response = None
+        threading.Thread.__init__(self)
 
-# class RetrievePackageNames(threading.Thread):
-#     def __init__(self,timeout):
-#         self.timeout = timeout
-#         self.result = None
-#         threading.Thread.__init__(self)
-
-#     def run(self):
-#         try:
-#             request = urllib.request.Request("https://www.myget.org/F/aspnetrelease/api/v2/Packages()?$select=Id&$format=json&orderby=DownloadCount&$top=10",
-#                 headers={"User-Agent": "Sublime"})
-#             http_file = urllib.request.urlopen(request, timeout=self.timeout)
-#             self.result = []
-#             response = json.loads(http_file.read().decode('utf-8'))['d']
-#             for package in response:
-#                 self.result.append(package['Id'])
-#             print(self.result)
-#             return
-#         except (urllib.request.HTTPError) as e:
-#             self.message = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
-#         except (urllib.request.URLError) as e:
-#             self.message = '%s: URL error %s contacting API' % (__name__, str(e.reason))
-#         self.result = False
-#         return
+    def run(self):
+        try:
+            request = urllib.request.Request("https://www.myget.org/F/aspnetrelease/api/v2/Packages()?"
+                + "$select=Id&"
+                + "$format=json&"
+                + "orderby=DownloadCount&"
+                + "$top=100",
+                headers={"User-Agent": "Sublime"})
+            http_file = urllib.request.urlopen(request, timeout=self.timeout)
+            self.response = json.loads(http_file.read().decode('utf-8'))['d']
+            return
+        except (urllib.request.HTTPError) as e:
+            self.message = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+        except (urllib.request.URLError) as e:
+            self.message = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+        self.response = False
+        return
