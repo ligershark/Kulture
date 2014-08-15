84ff119d0480a86f7b19f3962e5822ffef231718
Pradeep Gowda <pradeep@btbytes.com>
2014-08-15 12:47:45 -0400

    so that the build script can work on Linux
    Replace Mac specific `/Users/$USER` path with generic `$HOME`

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Add K to path and trigger build
 ver=`cat ~/.kre/alias/default.alias`
-add_to_path="/Users/"$USER"/.kre/packages/"$ver"/bin"
+add_to_path=$HOME"/.kre/packages/"$ver"/bin"
 export PATH=$PATH:/usr/local/bin:$add_to_path
 directory="./"
 temp=$directory"project.json"
